33601a1ef3a0a04f7b53155c919de98b51c490a7
VestnikAUE <72875075+VestnikAUE@users.noreply.github.com>
2023-02-27 16:42:39 +0500

    Add files via upload

---
+++ CONFIG.h
@@ -0,0 +1,72 @@
+#ifndef _CONFIG_H
+#define	_CONFIG_H
+
+/*
+// PIC16F630 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+#pragma config FOSC = XT       // Oscillator Selection bits (LP oscillator: Low power crystal on RA4/OSC2/CLKOUT and RA5/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
+#pragma config MCLRE = ON      // RA3/MCLR pin function select (RA3/MCLR pin function is digital I/O, MCLR internally tied to VDD)
+#pragma config BOREN = OFF      // Brown-out Detect Enable bit (BOD disabled)
+#pragma config CP = OFF          // Code Protection bit (Program Memory code protection is enabled)
+#pragma config CPD = OFF         // Data Code Protection bit (Data memory code protection is enabled)
+*/
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+
+// PIC16F15323 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1
+#pragma config FEXTOSC = OFF    // External Oscillator mode selection bits (Oscillator not enabled)
+#pragma config RSTOSC = HFINT32 // Power-up default value for COSC bits (HFINTOSC with OSCFRQ= 32 MHz and CDIV = 1:1)
+#pragma config CLKOUTEN = ON    // Clock Out Enable bit (CLKOUT function is enabled; FOSC/4 clock appears at OSC2)
+#pragma config CSWEN = OFF      // Clock Switch Enable bit (The NOSC and NDIV bits cannot be changed by user software)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (FSCM timer disabled)
+
+// CONFIG2
+#pragma config MCLRE = ON       // Master Clear Enable bit (MCLR pin is Master Clear function)
+#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
+#pragma config LPBOREN = ON     // Low-Power BOR enable bit (ULPBOR enabled)
+#pragma config BOREN = ON       // Brown-out reset enable bits (Brown-out Reset Enabled, SBOREN bit is ignored)
+#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (VBOR) set to 1.9V on LF, and 2.45V on F Devices)
+#pragma config ZCD = ON         // Zero-cross detect disable (Zero-cross detect circuit is always enabled)
+#pragma config PPS1WAY = ON     // Peripheral Pin Select one-way control (The PPSLOCK bit can be cleared and set only once in software)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable bit (Stack Overflow or Underflow will cause a reset)
+
+// CONFIG3
+#pragma config WDTCPS = WDTCPS_0// WDT Period Select bits (Divider ratio 1:32)
+#pragma config WDTE = OFF       // WDT operating mode (WDT Disabled, SWDTEN is ignored)
+#pragma config WDTCWS = WDTCWS_6// WDT Window Select bits (window always open (100%); no software control; keyed access required)
+#pragma config WDTCCS = HFINTOSC// WDT input clock selector (WDT reference clock is the 31.25 kHz HFINTOSC)
+
+// CONFIG4
+#pragma config BBSIZE = BB512   // Boot Block Size Selection bits (512 words boot block size)
+#pragma config BBEN = OFF       // Boot Block Enable bit (Boot Block disabled)
+#pragma config SAFEN = OFF      // SAF Enable bit (SAF disabled)
+#pragma config WRTAPP = OFF     // Application Block Write Protection bit (Application Block not write protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot Block not write protected)
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration Register not write protected)
+#pragma config WRTSAF = OFF     // Storage Area Flash Write Protection bit (SAF not write protected)
+#pragma config LVP = ON         // Low Voltage Programming Enable bit (Low Voltage programming enabled. MCLR/Vpp pin function is MCLR.)
+
+// CONFIG5
+#pragma config CP = OFF         // UserNVM Program memory code protection bit (UserNVM code protection disabled)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+
+//#include "EEPROM.h"
+#include "funcs.h"
+#include "i2c.h"
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
