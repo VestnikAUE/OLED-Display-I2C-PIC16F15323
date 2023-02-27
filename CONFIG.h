1a517f1e2f352cb8b35865d9538afda8346e3786
VestnikAUE <72875075+VestnikAUE@users.noreply.github.com>
2023-02-27 17:07:33 +0500

    Update CONFIG.h

--- CONFIG.h
+++ CONFIG.h
@@ -1,24 +1,6 @@
 #ifndef _CONFIG_H
 #define	_CONFIG_H
 
-/*
-// PIC16F630 Configuration Bit Settings
-
-// 'C' source line config statements
-
-// CONFIG
-#pragma config FOSC = XT       // Oscillator Selection bits (LP oscillator: Low power crystal on RA4/OSC2/CLKOUT and RA5/OSC1/CLKIN)
-#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
-#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
-#pragma config MCLRE = ON      // RA3/MCLR pin function select (RA3/MCLR pin function is digital I/O, MCLR internally tied to VDD)
-#pragma config BOREN = OFF      // Brown-out Detect Enable bit (BOD disabled)
-#pragma config CP = OFF          // Code Protection bit (Program Memory code protection is enabled)
-#pragma config CPD = OFF         // Data Code Protection bit (Data memory code protection is enabled)
-*/
-// #pragma config statements should precede project file includes.
-// Use project enums instead of #define for ON and OFF.
-
-
 // PIC16F15323 Configuration Bit Settings
 
 // 'C' source line config statements
@@ -64,7 +46,6 @@
 
 #include <xc.h>
 
-//#include "EEPROM.h"
 #include "funcs.h"
 #include "i2c.h"
 
